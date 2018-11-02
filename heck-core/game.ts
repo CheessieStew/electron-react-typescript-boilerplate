e67e1cf7e931ed80967a654afeea1cbfcbaf5ea7
CheessieStew <serindrack@gmail.com>
2018-11-02 17:32:55 +0100

    a

--- heck-core/game.ts
+++ heck-core/game.ts
@@ -1,5 +1,16 @@
 import {heckParser} from "./Evaluator/heckParser"
+import { heckLexer } from "./Evaluator/heckLexer";
 export function doStuff(x: number) {
-  console.log(heckParser.prototype.column_name())
-  return x + 5;
+  import { ANTLRInputStream, CommonTokenStream } from 'antlr4ts';
+
+  // Create the lexer and parser
+  let inputStream = new ANTLRInputStream("text");
+  let lexer = new heckLexer(inputStream);
+  let tokenStream = new CommonTokenStream(lexer);
+  let parser = new heckParser(tokenStream);
+  
+  // Parse the input, where `compilationUnit` is whatever entry point you defined
+  let result = parser.parse(); 
+  console.log(result);
+   return x + 5;
 }
